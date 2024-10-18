e0490be0e46592f485c874abb75cb495aedbdfab
Laurent Couturier <laurent_couturier@connect-tech.sncf>
2024-10-18 15:44:44 +0200

    refactor: :art: improve readibility

--- lib/src/rules/avoid_nested_record_rule.dart
+++ lib/src/rules/avoid_nested_record_rule.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: cascade_invocations, avoid_unused_constructor_parameters, unused_element, unused_import
 
-import 'dart:developer';
-
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/type.dart';
@@ -9,10 +7,10 @@ import 'package:analyzer/error/error.dart';
 import 'package:analyzer/error/listener.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 
-class AvoiddNestedRecordRule extends DartLintRule {
+class AvoidNestedRecordRule extends DartLintRule {
   static const lintName = 'avoid_nested_record';
 
-  const AvoiddNestedRecordRule()
+  const AvoidNestedRecordRule()
       : super(
           code: const LintCode(
             name: lintName,
@@ -37,11 +35,13 @@ class AvoiddNestedRecordRule extends DartLintRule {
     context.registry.addTypeAlias((node) {
       if (node is! GenericTypeAlias) return;
 
-      final n = node.declaredElement as TypeAliasElement;
-      if (n.aliasedType is! RecordType) return;
-      final r = n.aliasedType as RecordType;
-      bool hasNestedRecord = r.positionalFields.any((e) => e.type is RecordType);
-      if (!hasNestedRecord) return;
+      final element = node.declaredElement as TypeAliasElement;
+      if (element.aliasedType is! RecordType) return;
+
+      final record = element.aliasedType as RecordType;
+      bool isNested = record.positionalFields.any((e) => e.type is RecordType);
+      if (!isNested) return;
+
       reporter.reportErrorForNode(code, node);
     });
   }

216b3c0399f7de02c0e37b2eb49a8701b7284846
Laurent Couturier <laurent_couturier@connect-tech.sncf>
2024-11-04 09:42:32 +0100

    refactor: :zap: update avoid dynamic rule

--- lib/src/rules/avoid_dynamic_rule.dart
+++ lib/src/rules/avoid_dynamic_rule.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: unnecessary_cast, cascade_invocations, unused_element, unused_import
 
+import 'dart:developer';
+
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/error/error.dart';
@@ -9,18 +11,23 @@ import 'package:my_custom_lints/src/common/extensions.dart';
 import 'package:my_custom_lints/src/common/lint_rule_node_registry_extensions.dart';
 
 class AvoidDynamicRule extends DartLintRule {
+  static const ruleName = 'avoid_dynamic';
+
   const AvoidDynamicRule()
       : super(
           code: const LintCode(
-            name: 'avoid_dynamic',
+            name: ruleName,
             correctionMessage: 'Avoid using dynamic.',
             errorSeverity: ErrorSeverity.WARNING,
             problemMessage: 'Using dynamic is considered unsafe since it can easily result in runtime errors.',
           ),
         );
 
-  static const codeAddVoid =
-      LintCode(name: 'avoid_dynamic', errorSeverity: ErrorSeverity.WARNING, problemMessage: 'Add void type.');
+  static List<bool Function(NamedType node)> _rules = [
+    (e) => e.type == null,
+    (e) => e.type is DynamicType,
+    (e) => e.type.toString() == 'dynamic',
+  ];
 
   @override
   void run(
@@ -28,45 +35,22 @@ class AvoidDynamicRule extends DartLintRule {
     ErrorReporter reporter,
     CustomLintContext context,
   ) {
+    context.registry.addNamedType((node) {
+      if (!_rules.any((element) => element(node))) return;
+
+      reporter.reportErrorForNode(code, node);
+    });
+
     context.registry.addReturnType((node, parent) {
-      if (node == null) {
-        reporter.reportErrorForNode(codeAddVoid, parent);
-        return;
-      }
-      if (node.type == null) {
-        reporter.reportErrorForNode(code, node);
-        return;
-      }
-      if (node.type is DynamicType) {
-        reporter.reportErrorForNode(code, node);
-        return;
-      }
-      if (node.type.toString() == 'dynamic') {
-        reporter.reportErrorForNode(code, node);
-        return;
-      }
+      if (node != null) return;
 
-      if (node.type is RecordType) {
-        for (final field in (node.type! as RecordType).positionalFields) {
-          if (field.type is DynamicType) reporter.reportErrorForNode(code, node);
-        }
-        for (final field in (node.type! as RecordType).namedFields) {
-          if (field.type is DynamicType) reporter.reportErrorForNode(code, node);
-        }
-        return;
-      }
-    });
-
-    context.registry.addFormalParameterList((node) {
-      for (final p in node.parameters.where((e) => e.isDynamic)) {
-        reporter.reportErrorForNode(code, p);
-      }
-    });
-
-    context.registry.addVariableDeclaration((node) {
-      if (node.declaredElement?.type is DynamicType) {
-        reporter.reportErrorForNode(code, node);
-      }
+      reporter.reportErrorForNode(
+          LintCode(
+            name: ruleName,
+            errorSeverity: ErrorSeverity.WARNING,
+            problemMessage: 'Add void type.',
+          ),
+          parent);
     });
   }
 }
